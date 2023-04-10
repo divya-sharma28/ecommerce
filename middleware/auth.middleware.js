79ad8e0fae869accbc2cd4d2df92c1577641377b
divya-sharma28 <dms28052000@gmail.com>
2023-04-10 19:55:10 +0530

    added

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,28 @@
+import jwt from 'jsonwebtoken';
+
+export const auth =(req,res, next)=>{
+    try {
+        if(req.headers.authorization){
+            const token = req.headers.authorization ;
+            const decodeToken = jwt.verify(token,'stories123');
+            if(decodeToken){
+                next();
+            }
+            else{
+                res.status(401).json({
+                    message:'Invalid token'
+                })
+            }
+        }
+        else{
+            res.status(401).json({
+                message:'Token not found!'
+            })
+        }
+
+    } catch (error) {
+        res.status(500).json({
+            message:`Server Error: ${error.message}`
+        })
+    }
+}
--- routers/product.router.js
+++ routers/product.router.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import { addProduct,allProducts,singleProduct,updateProduct,deleteProduct} from '../controllers/product.controller';
+import {auth} from '../middleware/auth.middleware'
+
+
 const productRouter = express.Router();
 
+
 productRouter.post('/add-product',addProduct);
-productRouter.get('/get-product',allProducts);
-productRouter.get('/get-product/:prodID',singleProduct);
+productRouter.get('/get-product',auth,allProducts);
+productRouter.get('/get-product/:prodID',auth,singleProduct);
 productRouter.patch('/update-product/:prodID',updateProduct);
 productRouter.delete('/delete-product/:prodID',deleteProduct)
 
